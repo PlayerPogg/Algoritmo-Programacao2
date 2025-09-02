4f36beb3959743df3f55ae51f7cc08b84c0bb7bc
PlayerPogg <nandonunesfrancisco@gmail.com>
2025-09-01 22:13:26 -0300

    Create MODULvet5INCOMPLETO.cpp

---
+++ MODULvet5INCOMPLETO.cpp
@@ -0,0 +1,26 @@
+/*
+5. Elabore uma função sem retorno (procedimento) que receba um vetor X de n (1  n  10)
+elementos inteiros e devolva um vetor Y contendo apenas os elementos primos de X. Crie um
+programa para testar esta função. 
+*/
+#include<iostream>
+using namespace std;
+void constroiVetPrimos(int, int, int);
+
+int main()
+{
+    
+    return 0;
+}
+void constroiVetPrimos(int n, int X, int &m, int Y){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<X[i]/2; j++){
+            if(X[i]/)
+        }
+    }
+}
+bool ehPrimo(int n, int vetor[];){
+    for(int i=0; i<vetor[i]/2; i++){
+        if vetor[i]/i
+    }
+}
